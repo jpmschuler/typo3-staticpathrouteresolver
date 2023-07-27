ef78f7a722b161da373ffe36ccab11e8dbdb04d5
J. Peter M. Schuler <j.peter.m.schuler@uni-due.de>
2023-07-27 10:31:07 +0200

    [TASK] sync requirements between composer and ext_emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,8 +10,8 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => '',
     'constraints' => [
         'depends' => [
-            'php' => '7.4.0-8.99.99',
-            'typo3' => '11.5.0-12.4.99'
+            'php' => '7.4.0-8.2.99',
+            'typo3' => '11.5.0-11.5.99'
         ]
     ],
     'autoload' => [
